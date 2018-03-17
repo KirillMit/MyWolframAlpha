1d812d9c8e7deab8e71f721bba1a935c2b91b7f9
KirillMit <37476233+KirillMit@users.noreply.github.com>
2018-03-17 18:08:14 +0300

    Add files via upload

--- MitsikLib.h
+++ MitsikLib.h
@@ -8,6 +8,8 @@ double GrafY (double Y);
 double GrafX (double X);
 
 void Line (double X1, double Y1, double X2, double Y2);
+void Circle (double X, double Y, double R);
+void Axis (double X, double Y, double lx, double ly);
 
 //-----------------------------------------------------------------------------
 
@@ -35,4 +37,16 @@ void Line (double X1, double Y1, double X2, double Y2)
 void Circle (double X, double Y, double R)
     {
        txCircle (GrafX(X), GrafY(Y), R);
+    }
+
+//-----------------------------------------------------------------------------
+void Axis (double X, double Y, double lx, double ly)
+    {
+        txLine (GrafX(X - lx/2), GrafY(Y), GrafX(X + lx/2), GrafY(Y));
+        txLine (GrafX(X), GrafY(Y - ly/2), GrafX(X), GrafY(Y + ly/2));
+
+        txLine (GrafX(X + lx/2), GrafY(Y), GrafX(X + lx/2 - 20), GrafY(Y + 10));
+        txLine (GrafX(X + lx/2), GrafY(Y), GrafX(X + lx/2 - 20), GrafY(Y - 10));
+        txLine (GrafX(X), GrafY(Y + ly/2), GrafX(X + 10), GrafY(Y + ly/2 - 20));
+        txLine (GrafX(X), GrafY(Y + ly/2), GrafX(X - 10), GrafY(Y + ly/2 - 20));
     }
