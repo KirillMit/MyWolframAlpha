1b8aa80b191b3dc987222d3fd918242f96c3ddb6
KirillMit <37476233+KirillMit@users.noreply.github.com>
2018-03-24 16:47:27 +0300

    Синус, косинус, оси

--- MitsikLib.h
+++ MitsikLib.h
@@ -10,10 +10,12 @@ double GrafX (double X);
 void Line (double X1, double Y1, double X2, double Y2);
 void Circle (double X, double Y, double R);
 void Axis (double X, double Y, double lx, double ly);
+void cosius(double x, double y);
+void cosius(double x, double y);
 
 //-----------------------------------------------------------------------------
 
-double GraphSizeX = 1, GraphSizeY = 1;
+double GraphSizeX = 5, GraphSizeY = 5;
 
 //=============================================================================
 double GrafX (double X)
@@ -44,9 +46,27 @@ void Axis (double X, double Y, double lx, double ly)
     {
         txLine (GrafX(X - lx/2), GrafY(Y), GrafX(X + lx/2), GrafY(Y));
         txLine (GrafX(X), GrafY(Y - ly/2), GrafX(X), GrafY(Y + ly/2));
+    }
 
-        txLine (GrafX(X + lx/2), GrafY(Y), GrafX(X + lx/2 - 20), GrafY(Y + 10));
-        txLine (GrafX(X + lx/2), GrafY(Y), GrafX(X + lx/2 - 20), GrafY(Y - 10));
-        txLine (GrafX(X), GrafY(Y + ly/2), GrafX(X + 10), GrafY(Y + ly/2 - 20));
-        txLine (GrafX(X), GrafY(Y + ly/2), GrafX(X - 10), GrafY(Y + ly/2 - 20));
+//-----------------------------------------------------------------------------
+void cosinus(double x, double y)
+    {
+    while(x < txGetExtentX() / 2)
+        {
+            y = cos(x);
+            Circle (x, y, 2);
+            Circle (-x, y, 2);
+            x = x + 0.001;
+        }
+    }
+//-----------------------------------------------------------------------------
+void sinus(double x, double y)
+    {
+    while(x < txGetExtentX() / 2)
+        {
+            y = sin(x);
+            Circle (x, y, 2);
+            Circle (-x, -y, 2);
+            x = x + 0.001;
+        }
     }
