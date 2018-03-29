71f574b5e02984ddd38cb345d4be21db5befdc79
KirillMit <37476233+KirillMit@users.noreply.github.com>
2018-03-29 17:41:21 +0300

    void Grafic 1

---
+++ KM.h
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+#include <conio.h>
+#include "TXLib.h"
+
+double GrafY (double Y);
+double GrafX (double X);
+
+void Line (double X1, double Y1, double X2, double Y2);
+void Circle (double X, double Y, double R);
+void Axis (double X, double Y, double lx, double ly);
+void cosinus(double x, double y);
+void sinus(double x, double y);
+void Grafik(double x, double y, char k);
+
+
+//-----------------------------------------------------------------------------
+
+double GraphSizeX = 15, GraphSizeY = 15;
+
+//=============================================================================
+double GrafX (double X)
+    {
+    return (GraphSizeX * X + txGetExtentX() / 2);
+    }
+
+//-----------------------------------------------------------------------------
+double GrafY (double Y)
+    {
+    return (txGetExtentY() / 2 - GraphSizeY * Y);
+    }
+
+//-----------------------------------------------------------------------------
+void Line (double X1, double Y1, double X2, double Y2)
+    {
+       txLine (GrafX(X1), GrafY(Y1), GrafX(X2), GrafY(Y2));
+    }
+
+//-----------------------------------------------------------------------------
+void Circle (double X, double Y, double R)
+    {
+       txCircle (GrafX(X), GrafY(Y), R);
+    }
+
+//-----------------------------------------------------------------------------
+void Axis (double X, double Y, double lx, double ly)
+    {
+        txLine (GrafX(X - lx/2), GrafY(Y), GrafX(X + lx/2), GrafY(Y));
+        txLine (GrafX(X), GrafY(Y - ly/2), GrafX(X), GrafY(Y + ly/2));
+    }
+
+//-----------------------------------------------------------------------------
+void cosinus(double x, double y)
+    {
+    while(x < txGetExtentX() / 2)
+        {
+            y = cos(x);
+            Circle (x, y, 2);
+            Circle (-x, y, 2);
+            x = x + 0.001;
+        }
+    }
+//-----------------------------------------------------------------------------
+void sinus(double x, double y)
+    {
+    while(x < txGetExtentX() / 2)
+        {
+            y = sin(x);
+            Circle (x, y, 2);
+            Circle (-x, -y, 2);
+            x = x + 0.001;
+        }
+    }
+//-----------------------------------------------------------------------------
+void Grafik(double x, double y, char k)
+    {
+        if (k == 115)
+        {
+            while(x < txGetExtentX() / 2)
+            {
+                y = sin(x);
+                Circle (x, y, 2);
+                Circle (-x, -y, 2);
+                x = x + 0.001;
+            }
+        }
+        else if (k == 99)
+        {
+            while(x < txGetExtentX() / 2)
+            {
+                y = cos(x);
+                Circle (x, y, 2);
+                Circle (-x, y, 2);
+                x = x + 0.001;
+            }
+        }
+    }
