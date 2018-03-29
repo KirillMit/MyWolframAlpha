893d99ac65e14efd27ca4b611fc4d5eeee6fd873
Ilya Dedinsky <ded32@users.noreply.github.com>
2018-03-29 18:22:50 +0300

    Changes proposed

--- KM.h
+++ KM.h
@@ -14,36 +14,40 @@ void cosinus(double x, double y);
 void sinus(double x, double y);
 void Grafik(double x, double y, char k);
 
-
 //-----------------------------------------------------------------------------
 
 double GraphSizeX = 15, GraphSizeY = 15;
 
 //=============================================================================
+
 double GrafX (double X)
     {
     return (GraphSizeX * X + txGetExtentX() / 2);
     }
 
 //-----------------------------------------------------------------------------
+
 double GrafY (double Y)
     {
     return (txGetExtentY() / 2 - GraphSizeY * Y);
     }
 
 //-----------------------------------------------------------------------------
+
 void Line (double X1, double Y1, double X2, double Y2)
     {
        txLine (GrafX(X1), GrafY(Y1), GrafX(X2), GrafY(Y2));
     }
 
 //-----------------------------------------------------------------------------
+
 void Circle (double X, double Y, double R)
     {
        txCircle (GrafX(X), GrafY(Y), R);
     }
 
 //-----------------------------------------------------------------------------
+
 void Axis (double X, double Y, double lx, double ly)
     {
         txLine (GrafX(X - lx/2), GrafY(Y), GrafX(X + lx/2), GrafY(Y));
@@ -51,6 +55,7 @@ void Axis (double X, double Y, double lx, double ly)
     }
 
 //-----------------------------------------------------------------------------
+
 void cosinus(double x, double y)
     {
     while(x < txGetExtentX() / 2)
@@ -61,7 +66,9 @@ void cosinus(double x, double y)
             x = x + 0.001;
         }
     }
+
 //-----------------------------------------------------------------------------
+
 void sinus(double x, double y)
     {
     while(x < txGetExtentX() / 2)
@@ -72,10 +79,19 @@ void sinus(double x, double y)
             x = x + 0.001;
         }
     }
+
+// FIXME Вообще, это не очень хорошая идея - включать в функции циклы. Циклы при этом дублируются. В данном случае это неизбежно,
+// т.к. ты встроил в sinus() и cosinus() симметричность графиков sin и cos. В общем случае это не будет работать, поэтому
+// лучше за один оборот цикла рисовать одну точку, а не две. Тогда циклы у всех функций будут выглядеть одинаково, и if, выбирающий
+// sin или cos, можно загнать в цикл. В результате рисующая функция будет одна. Примеры ее вызовов:
+//     DrawGraphic (FUNC_SIN);
+//     DrawGraphic (FUNC_COS);
+
 //-----------------------------------------------------------------------------
+
 void Grafik(double x, double y, char k)
     {
-        if (k == 115)
+        if (k == 115) // FIXME Лучше 's' (это то же самое)
         {
             while(x < txGetExtentX() / 2)
             {
@@ -85,7 +101,8 @@ void Grafik(double x, double y, char k)
                 x = x + 0.001;
             }
         }
-        else if (k == 99)
+
+        else if (k == 99) // FIXME Еще лучше == FUNC_COS, где FUNC_COS - константа const int FUNC_COS = 'c' (объяви это сверху)
         {
             while(x < txGetExtentX() / 2)
             {
